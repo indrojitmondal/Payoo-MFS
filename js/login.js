708d547b4a69ee044d318c254e10c469ea0cbab7
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-18 22:56:12 +0600

    added home.html and setted credential to login

--- js/login.js
+++ js/login.js
@@ -4,11 +4,19 @@ document.getElementById('button-login')
 
     event.preventDefault();
 
-    console.log('login button clicked');
+    // console.log('login button clicked');
 
     const phone_number = document.getElementById('phone-number').value;
     console.log('Phone Number:',phone_number);
     const pin = document.getElementById('pin').value;
     console.log('Pin Number:',pin);
+    if(phone_number==='01577147931' && pin==='1234'){
+
+        window.location.href='./home.html';
+
+    }
+    else{
+        alert("Wrong Phone Number or Pin");
+    }
 
 })
